35bcdb7842d8d2bd01bf8e00bf5c059c4bd02ec3
Darun Mahavanakul <68772285+Navisk555@users.noreply.github.com>
2022-04-30 18:47:18 +0700

    Add files via upload

---
+++ backend/backend221/src/main/java/com/example/backend221/entities/Event.java
@@ -0,0 +1,87 @@
+package com.example.backend221.entities;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "event")
+public class Event {
+    @Id
+    @Column(name = "bookingId", nullable = false)
+    private Integer id;
+
+    @Column(name = "bookingEmail", nullable = false, length = 50)
+    private String bookingEmail;
+
+    @Column(name = "bookingName", nullable = false, length = 100)
+    private String bookingName;
+
+    @Column(name = "eventStartTime", nullable = false)
+    private Instant eventStartTime;
+
+    @Column(name = "eventNotes", length = 500)
+    private String eventNotes;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "eventCategory", nullable = false)
+    private EventCategory eventCategory;
+
+    @Column(name = "eventDuration", nullable = false)
+    private Integer eventDuration;
+
+    public Integer getEventDuration() {
+        return eventDuration;
+    }
+
+    public void setEventDuration(Integer eventDuration) {
+        this.eventDuration = eventDuration;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+
+    public String getEventNotes() {
+        return eventNotes;
+    }
+
+    public void setEventNotes(String eventNotes) {
+        this.eventNotes = eventNotes;
+    }
+
+    public Instant getEventStartTime() {
+        return eventStartTime;
+    }
+
+    public void setEventStartTime(Instant eventStartTime) {
+        this.eventStartTime = eventStartTime;
+    }
+
+    public String getBookingName() {
+        return bookingName;
+    }
+
+    public void setBookingName(String bookingName) {
+        this.bookingName = bookingName;
+    }
+
+    public String getBookingEmail() {
+        return bookingEmail;
+    }
+
+    public void setBookingEmail(String bookingEmail) {
+        this.bookingEmail = bookingEmail;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
