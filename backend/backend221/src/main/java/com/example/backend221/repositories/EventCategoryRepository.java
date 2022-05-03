07b9d6aa9fd92d2d853a2e34c40ac6810c55e28d
jubbyr <87378794+jubbyr@users.noreply.github.com>
2022-05-03 23:30:30 +0700

    Delete backend/backend221 directory

--- backend/backend221/src/main/java/com/example/backend221/repositories/EventCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend221.repositories;
-
-import com.example.backend221.entities.EventCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventCategoryRepository extends JpaRepository<EventCategory, Integer> {
-}
--- backend/backend221/src/main/java/com/example/backend221/repositories/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend221.repositories;
-
-import com.example.backend221.entities.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-}
