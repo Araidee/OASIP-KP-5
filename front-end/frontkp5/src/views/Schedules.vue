cbc6ac1d09438da13a5e148ab0d059ba183fbd3a
sarinweerakun <sarin.kobfha@mail.kmutt.ac.th>
2022-05-10 01:10:17 +0700

    Update Schedules.vue

--- front-end/frontkp5/src/views/Schedules.vue
+++ front-end/frontkp5/src/views/Schedules.vue
@@ -15,7 +15,7 @@ const checkEmptyArr = (arr) => {
 }
 //GET
 const getEvents = async () => {
-  const res = await fetch("http://10.4.56.88:8080/api/events")
+  const res = await fetch("http://202.44.9.103:8080/kp5/api/events")
   if (res.status === 200) {
     events.value = await res.json()
   } else console.log("Error, cannot get data");
