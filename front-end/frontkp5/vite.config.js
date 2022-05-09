fda7e5dd81f09eabbe06914d72ab96efdabd5de9
jubbyr <udomchai44@icloud.com>
2022-05-09 22:27:11 +0700

    Update vite.config.js

--- front-end/frontkp5/vite.config.js
+++ front-end/frontkp5/vite.config.js
@@ -4,15 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/kp5/'
-  ,
-  server: {
-    proxy: {
-      '/api' : {
-        target: 'http://10.4.56.88:8080/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+  base: '/kp5/',
+  // ,
+  // server: {
+  //   proxy: {
+  //     '/api' : {
+  //       target: 'http://10.4.56.88:8080/',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
+   proxy: {
+    '/api': {
+         target: 'https://localhost:44305',
+         changeOrigin: true,
+         secure: false,      
+         ws: true,
+     }
     }
-  }
 })
