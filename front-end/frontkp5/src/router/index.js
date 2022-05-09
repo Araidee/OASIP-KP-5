6e0fa5877c217666774cb350d2e67f86b40cb8fa
jubbyr <udomchai44@icloud.com>
2022-05-09 21:27:34 +0700

    try

--- front-end/frontkp5/src/router/index.js
+++ front-end/frontkp5/src/router/index.js
@@ -5,7 +5,7 @@ import NotFound from '../views/NotFound.vue'
 import Schedules from '../views/Schedules.vue'
 import Booking from '../views/Booking.vue'
 
-const history = createWebHistory('/kp5/');
+const history = createWebHistory(import.meta.env.BASE_URL);
 const routes = [
     {
         path: '/',
