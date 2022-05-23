2574e944937ac9db51f154672a2294011c005402
jubbyr <udomchai44@icloud.com>
2022-05-23 19:47:15 +0700

    add getbydate

--- backend/backend221/src/main/java/com/example/backend221/repositories/EventRepository.java
+++ backend/backend221/src/main/java/com/example/backend221/repositories/EventRepository.java
@@ -9,10 +9,14 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Integer> {
 //    @Query(value = "select * from EventCategory  where id = ?1",nativeQuery = true)
      List<Event> findEventByEventCategory_IdOrderByEventStartTimeDesc(Integer categoryId);
-     @Query(value = "select * from event where eventStartTime > :current_datetime order by eventStartTime",nativeQuery = true)
-        List<Event> findAllFutureEvents(@Param("current_datetime") String current_datetime);
-    @Query(value = "select * from event where eventStartTime <= :current_datetime order by eventStartTime",nativeQuery = true)
-    List<Event> getAllPastEvents(@Param("current_datetime")String current_datetime);
 
+        @Query(value = "select * from event where eventStartTime > :current_datetime order by eventStartTime",nativeQuery = true)
+        List<Event> findAllFutureEvents(@Param("current_datetime") String current_datetime);
+
+        @Query(value = "select * from event where eventStartTime <= :current_datetime order by eventStartTime desc",nativeQuery = true)
+        List<Event> getAllPastEvents(@Param("current_datetime")String current_datetime);
+
+        @Query(value = "select * from event where DATE_FORMAT(eventStartTime,'%Y-%m-%d')=:justDate order by eventStartTime desc",nativeQuery = true)
+        List<Event> getByDay(@Param("justDate") String justDate);
 
 }
