90b6798605eaf60a734a8a69f0c4314574d48e04
jubbyr <udomchai44@icloud.com>
2022-09-24 23:55:50 +0700

    Update WebSecurityConfig.java

--- backend/backend221/src/main/java/com/example/backend221/config/WebSecurityConfig.java
+++ backend/backend221/src/main/java/com/example/backend221/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
                     .authorizeRequests()
                     .antMatchers("/api/match","/api/match/*").permitAll()
                     .antMatchers("/api/events","/api/events/*").permitAll()
-                    .antMatchers("/api/login/*").permitAll()
+                    .antMatchers("/api/login/","/api/login/*").permitAll()
 
 //                    .antMatchers("/api/users").authenticated()
 //                    .anyRequest("/users").authenticated()
